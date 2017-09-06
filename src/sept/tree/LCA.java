2c2ec4afa9ce143754c492ecc90d877da90b5fe6
xbog <80210534@qq.com>
2017-09-06 10:52:27 +0800

    最近公共祖先：

--- src/sept/tree/LCA.java
+++ src/sept/tree/LCA.java
@@ -4,53 +4,64 @@ import alg.tree.TNode;
 
 /**
  * Created by Administrator on 2016/12/15.
- * 最近公共祖先
- *
- * 1，分配任务是：
- * 返回祖先节点，
- * 压任务的终点：
- * 1，如果为空，返回null;
- * 2,如果找到了O1，就返回O1,也就是他本身
- *
- * 本级要做的事情是：
- *
- * 不用做
- *
- * 收任务要做的事情是：
- *
- * 1、left，right 如果都不为空，说明找到了两边，返回head
- * 1机械键盘怎么样好不好用你觉得怎么红轴的一看就很垃圾
- *
+    写一个cover：判断当前节点是在属于 该子树上面
  *
  *
  */
 public class LCA {
 
+
+    /**
+     * 判断当前节点是不是属于这棵树
+     * @param root
+     * @param node
+     * @return
+     */
+    public static boolean isExist(TNode root ,TNode node){
+        if (root == null){
+            return false;
+        }
+        if (node == null){
+            return true;
+        }
+
+        //如果当前节点相等了:这是最底层的计算了。
+        if (root == node){
+            return true;
+        }
+
+        //回收任务结果，如果左右孩子有一个是存在的，那么就是存在的
+        boolean left = isExist(root.left,node);
+        boolean right = isExist(root.left,node);
+
+        return left || right;
+    }
+
+
+    /**
+     * 如果当前节点都在左子树，那么需要往下找，
+     * 如果不是在一边的话，就是当前节点了
+     * @param root
+     * @param t1
+     * @param t2
+     * @return
+     */
     public static TNode getAncenstor(TNode root,TNode t1,TNode t2){
         if (root == null){
             return null;
         }
 
-        //只要有一个是节点是根节点，返回当前
-        if (root == t1 || root == t2){
-            return root;
+        if (isExist(root.left,t1) && isExist(root.left,t2)){
+            return getAncenstor(root.left,t1,t2);
         }
 
-        //本级要做的事情是：无
-        //本级要返回的事情是：
-        TNode left =getAncenstor(root.left,t1,t2);
-        TNode right =getAncenstor(root.right,t1,t2);
-
-        //左右孩子树的公共祖先节点：分为三种 1两个都是空，
-        if (left != null && right !=null){
-            return root;
+        if (isExist(root.right,t1) && isExist(root.right,t2)){
+            return getAncenstor(root.right,t1,t2);
         }
 
-        if (left !=null){
-            return left;
-        }
+        //如果都不是就是root
+        return root;
 
-        return right;
     }
 
     public static void main(String[] args) {
