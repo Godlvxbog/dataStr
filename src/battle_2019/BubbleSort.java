70ab849c0857dca753dc48f1bd9d37f42e58aba5
xbog <1969084327@qq.com>
2019-05-26 19:17:13 +0800

    fixed

--- src/battle_2019/BubbleSort.java
+++ src/battle_2019/BubbleSort.java
@@ -1,7 +1,7 @@
 package battle_2019;
 
 /**
- * 待排序的数字在i的后面，i--temp
+ * 待排序的数字在i的后面，i--temp，上浮冒泡排序，从后面往前面排序
  */
 public class BubbleSort {
     public static void swap(int[] nums, int start, int end){
@@ -22,7 +22,7 @@ public class BubbleSort {
     public static void bubbleSort(int[] nums){
         for (int i= 0;i < nums.length;i++){
             for (int j= nums.length-1;j>i;j--){
-                if (nums[j]< nums[i]){
+                if (nums[j]< nums[j-1]){//后面的比前面还要大，就交换
                     swap(nums,i,j);
                 }
             }
